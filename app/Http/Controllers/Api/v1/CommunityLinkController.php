753892cf332915a66f2bf7ca0a79792b640c9a90
JeffreyArrosio <jeffrey.aj98@gmail.com>
2024-11-26 21:42:28 +0000

    Rutas modificadas

--- app/Http/Controllers/Api/v1/CommunityLinkController.php
+++ app/Http/Controllers/Api/v1/CommunityLinkController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers\Api\v1;
 use Illuminate\Http\Request;
 use Orion\Http\Controllers\Controller;
 use App\Models\Centro;
+use Orion\Concerns\DisableAuthorization;
 
 class CommunityLinkController extends Controller
 {
+    use DisableAuthorization;
     protected $model = Centro::class;
 }
