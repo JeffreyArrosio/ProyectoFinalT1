22bc131cbae02d31991fd8e3cedc9bc1d0030c18
Yeray <yeraygarciamartinez5@gmail.com>
2024-12-02 17:42:56 +0000

    Cambios visuales(hize un git stash ya que estava en la rama Developer)

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -1,21 +1,18 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight text-center">
             {{ $user->name ?? __('Show') . " " . __('User') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-12 flex justify-center">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-6 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="w-full">
-                    <div class="sm:flex sm:items-center">
-                        <div class="sm:flex-auto">
-                            <h1 class="text-lg font-semibold leading-6 text-gray-900 dark:text-gray-100">{{ __('Show') }} User</h1>
-                            <p class="mt-2 text-sm text-gray-700 dark:text-gray-300">Details of {{ __('User') }}.</p>
-                        </div>
-                        <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-                            <a type="button" href="{{ route('users.index') }}" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Back</a>
+                    <div class="sm:flex sm:items-center justify-center">
+                        <div class="sm:flex-auto text-center">
+                            <h1 class="text-lg font-semibold leading-6 text-gray-900 dark:text-gray-100">Usuario</h1>
+                            <p class="mt-2 text-sm text-gray-700 dark:text-gray-300">Detalles del usuario.</p>
                         </div>
                     </div>
 
@@ -25,23 +22,26 @@
                                 <div class="mt-6 border-t border-gray-200 dark:border-gray-700">
                                     <dl class="divide-y divide-gray-200 dark:divide-gray-700">
                                         <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                            <dt class="text-sm font-medium leading-6 text-gray-900 dark:text-gray-100">Name</dt>
+                                            <dt class="text-sm font-medium leading-6 text-gray-900 dark:text-gray-100">Nombre:</dt>
                                             <dd class="mt-1 text-sm leading-6 text-gray-700 dark:text-gray-300 sm:col-span-2 sm:mt-0">{{ $user->name }}</dd>
                                         </div>
                                         <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                            <dt class="text-sm font-medium leading-6 text-gray-900 dark:text-gray-100">Email</dt>
+                                            <dt class="text-sm font-medium leading-6 text-gray-900 dark:text-gray-100">Email:</dt>
                                             <dd class="mt-1 text-sm leading-6 text-gray-700 dark:text-gray-300 sm:col-span-2 sm:mt-0">{{ $user->email }}</dd>
                                         </div>
                                         <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                            <dt class="text-sm font-medium leading-6 text-gray-900 dark:text-gray-100">Admin</dt>
-                                            <dd class="mt-1 text-sm leading-6 text-gray-700 dark:text-gray-300 sm:col-span-2 sm:mt-0">{{ $user->admin }}</dd>
+                                            <dt class="text-sm font-medium leading-6 text-gray-900 dark:text-gray-100">Admin:</dt>
+                                            <dd class="mt-1 text-sm leading-6 text-gray-700 dark:text-gray-300 sm:col-span-2 sm:mt-0">{{ $user->admin ? 'Sí' : 'No' }}</dd>
                                         </div>
                                         <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                            <dt class="text-sm font-medium leading-6 text-gray-900 dark:text-gray-100">Centros Id</dt>
+                                            <dt class="text-sm font-medium leading-6 text-gray-900 dark:text-gray-100">Id Centro:</dt>
                                             <dd class="mt-1 text-sm leading-6 text-gray-700 dark:text-gray-300 sm:col-span-2 sm:mt-0">{{ $user->centros_id }}</dd>
                                         </div>
                                     </dl>
                                 </div>
+                                <a href="{{ route('users.index') }}" class="block mt-4 rounded-md bg-green-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-green-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                    Back
+                                </a>
                             </div>
                         </div>
                     </div>
